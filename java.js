4ca0366199e3c80660fe1f2b351f5f1e4bdedd66
augustoqz <132684730+augustoqz@users.noreply.github.com>
2024-02-26 11:41:18 -0300

    Update java.js

--- java.js
+++ java.js
@@ -1,6 +1,37 @@
 var pessoa = "832.192.304-34"
 
-console.log(pessoa);
+console.log(`Conteudo inicial variavel:  ${pessoa} | Tipo: ${typeof(pessoa)}`);
 pessoa = 259;
 
 console.log(`Mudei o conteúdo: ${pessoa} | Tipo: ${typeof(pessoa)}`);
+
+var num1 = 10
+var num2 = 20
+
+var soma = 0
+console.log(`Total é: ${soma}`);
+
+soma = num1 + num2;
+
+console.log(`Total da soma ${num1}+${num2}=${soma}`);
+
+var sub = 0
+console.log(`Total é: ${sub}`);
+
+sub = num1 - num2;
+
+console.log(`Total da subtração ${num1}-${num2}=${sub}`);
+
+var mul = 0
+console.log(`Total é: ${mul}`);
+
+mul = num1 * num2;
+
+console.log(`Total da multiplicação ${num1}*${num2}=${mul}`);
+
+var div = 0
+console.log(`Total é: ${div}`);
+
+div = num1 / num2;
+
+console.log(`Total da multiplicação ${num1}/${num2}=${div}`);
